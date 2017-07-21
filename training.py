d0c4c3f3c44b396862ad5f9edc4b3e33f1f4f407
S-H-Y-GitHub <932495000@qq.com>
2017-07-21 22:02:40 +0800

    使用with语句

--- training.py
+++ training.py
@@ -1,17 +1,14 @@
 import codecs
 import tensorflow as tf
 
-# 读取训练数据
-trainingFile = codecs.open("data/training.data", encoding='utf-8')
-lines = trainingFile.readlines()
-trainingFile.close()
-# 解析训练数据
-trainingSet = dict()
-for line in lines:
-    line = line.split('\t')
-    line[2] = float(line[2])
-    answer = dict([line[1:]])
-    if line[0] in trainingSet:
-        trainingSet[line[0]].append(answer)
-    else:
-        trainingSet[line[0]] = [answer]
+# 读取并解析训练数据
+with codecs.open("data/training.data", encoding='utf-8') as trainingFile:
+    trainingSet = dict()
+    for line in trainingFile:
+        line = line.split('\t')
+        line[2] = float(line[2])
+        answer = dict([line[1:]])
+        if line[0] in trainingSet:
+            trainingSet[line[0]].append(answer)
+        else:
+            trainingSet[line[0]] = [answer]
