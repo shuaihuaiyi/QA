f6ef2a07aeb47a0fa5ce29b8cec9d93242bdb641
S-H-Y-GitHub <932495000@qq.com>
2017-07-21 13:09:09 +0800

    文件操作完成

--- training.py
+++ training.py
@@ -3,5 +3,16 @@ import tensorflow as tf
 
 #读取训练数据
 trainingFile = codecs.open("data/training.data",encoding='utf-8')
-trainingSet = trainingFile.readlines()
+lines = trainingFile.readlines()
 trainingFile.close()
+#解析训练数据
+trainingSet = dict()
+for line in lines:
+    line = line.split('\t')
+    line[2] = int(line[2])
+    answer = dict([line[1:]])
+    if line[0] in trainingSet:
+        trainingSet[line[0]].append(answer)
+    else:
+        trainingSet[line[0]]=[answer]
+
