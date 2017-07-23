7b5c1ba116995f6e51217fc5e7a58305b00f1260
S-H-Y-GitHub <932495000@qq.com>
2017-07-23 23:22:35 +0800

    添加word2vec读取函数

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ import tensorflow as tf
 if __name__ == '__main__':
     trainingFile = "data/training.data"
     testFile = "data/develop.data"
+    embeddingFile = "word2vec/zhwiki_2017_03.sg_50d.word2vec"
+    embeddingSize = 50
+    readData.readEmbeddingFile(embeddingFile,embeddingSize)
--- readData.py
+++ readData.py
@@ -15,3 +15,23 @@ def readFile(filename):
             line[2] = float(line[2])
             result.append(line)
     return result
+
+
+def readEmbeddingFile(filename, embeddingSize):
+    """
+    读取训练好的word2vec文件，获取每个词语对应的向量
+
+    :param filename:文件名
+    :param embeddingSize:转换成的词向量的维度
+    :return:词语到词向量的映射字典，注意这个词典中未登录词的处理
+    """
+    word2idx = dict()
+    with open(filename, mode="r", encoding="utf-8") as rf:
+        for line in rf.readlines():
+            arr = line.split(" ")
+            if len(arr) != (embeddingSize + 2):
+                print("词向量维度定义有误")
+                continue
+            embedding = [float(val) for val in arr[1: -1]]
+            word2idx[arr[0]] = embedding
+    return word2idx
