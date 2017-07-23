7aacbaaa12feed29859b4c218c5b75fc706095c9
S-H-Y-GitHub <932495000@qq.com>
2017-07-24 00:22:57 +0800

    完成预处理

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ import readData
 import evaluation
 import tensorflow as tf
 
+
 if __name__ == '__main__':
-    #定义参数
+    # 定义参数
     trainingFile = "data/training.data"
     testFile = "data/develop.data"
     embeddingFile = "word2vec/zhwiki_2017_03.sg_50d.word2vec"
     embeddingSize = 50
+
+    # 读取数据
+    trainingList = readData.readFile(trainingFile)
+    testList = readData.readFile(testFile)
+    embeddingDict = readData.readEmbeddingFile(embeddingFile, embeddingSize)
+
+    # 预处理
+    trainingVec = readData.textToVec(trainingList, embeddingDict)
+    testVec = readData.textToVec(testList, embeddingDict)
